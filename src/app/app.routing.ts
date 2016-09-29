36a22a847097f3dbda9dd0c4a68c071d5c6bf34b
zeject <424049902@qq.com>
2016-09-29 21:55:33 +0800

    communication router +

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { authProviders, dashboardRoutes } from './dashboard/dashboard.routing';
 import { AuthGuard } from './+home/auth-guard.service';
 import { loadHomeRoutes } from './+home/home.routing';
-import { loadCommunicationRoutes } from './communication/communication.routing';
+import { loadCommunicationRoutes } from './+communication/communication.routing';
 
 const appRoutes: Routes = [
     ...dashboardRoutes,
